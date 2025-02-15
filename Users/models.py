6911b7e13dcbf1defb05b92f038a6bfa3c47e69b
jalil <95492487+leonardo0231@users.noreply.github.com>
2025-02-15 20:12:02 +0330

    Create Users app

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    date_joined = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.username
