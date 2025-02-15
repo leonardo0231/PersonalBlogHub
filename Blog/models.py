2e043ea26d395b838174f55a9ee08d0f986f8bde
jalil <95492487+leonardo0231@users.noreply.github.com>
2025-02-15 20:21:11 +0330

    Create Post and Comment models

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from Users.models import CustomUser
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment")
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username}: comment on {self.post.title}"
