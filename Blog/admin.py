9378f6f54afae47510625d3f05b9e2ff7201e373
jalil <95492487+leonardo0231@users.noreply.github.com>
2025-02-16 22:28:50 +0330

    admin pages

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+
+class AdminPost(admin.ModelAdmin):
+    list_display = ('title', 'author', 'create_at', 'update_at')
+    ordering = ('title', 'author', 'create_at')
+    list_filter = ('author', 'create_at')
+    search_fields = ('title', 'author')
+    readonly_fields = ('create_at',)
+
+
+class AdminComment(admin.ModelAdmin):
+    list_display = ('post', 'user')
+    list_filter = ('post', 'user', 'create_at')
+    search_fields = ('post', 'user')
+
+
+admin.site.register(Post, AdminPost)
+admin.site.register(Comment, AdminComment)
--- Users/admin.py
+++ Users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import CustomUser
 # Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "date_joined")
+    ordering = ("date_joined", "username",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
