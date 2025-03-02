2287ea43481d8d8f836177988ab7c8db08f8c8f8
jalil <95492487+leonardo0231@users.noreply.github.com>
2025-03-02 16:43:38 +0330

    add templates and static files

--- PersonalBlog/urls.py
+++ PersonalBlog/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("blog/", include("Blog.urls"), name="home"),
+    path("", home, name="home"),
     path("user/", include("Users.urls")),
 ]
 
