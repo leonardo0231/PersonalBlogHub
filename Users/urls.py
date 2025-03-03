9f5049be064b802db783a7b7ee700447244f9583
jalil <95492487+leonardo0231@users.noreply.github.com>
2025-03-03 20:09:16 +0330

    config urls and add some style for pages

--- Users/urls.py
+++ Users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register
+from .views import register, userInfo
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
+    path('', userInfo, name='userInfo'),
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
 
 # Create your views here.
@@ -18,5 +17,7 @@ def register(request):
 
     return render(request, "auth/register.html", {"form":form} )
 
+@login_required(login_url='/user/login/')
 def userInfo(request):
-    return HttpResponse("Hello") 
+    return render(request, "auth/userInfo.html", {"user":request.user})
+ 
